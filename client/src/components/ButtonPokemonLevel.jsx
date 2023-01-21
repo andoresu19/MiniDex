8c1ad517f2b3a4460ea3d109c4a914e9660228f1
andresbomo <andres.bomo02@gmail.com>
2023-01-21 09:41:00 -0500

    feat(buttonpokemonlevel): add dinamic style

--- client/src/components/ButtonPokemonLevel.jsx
+++ client/src/components/ButtonPokemonLevel.jsx
@@ -1,9 +1,16 @@
 import styles from "./ButtonPokemonLevel.module.css";
 
-export function ButtonPokemonLevel({order, sprite}) {
+export function ButtonPokemonLevel({ visible, order, sprite }) {
+  var buttonStyle = {};
+  if (visible) buttonStyle["display"] = "flex";
+  if (order == "next") buttonStyle["right"] = "-20px";
+  if (order == "back") buttonStyle["left"] = "-20px";
   return (
-    <div className={styles.content}>
-      <img className={styles.image} style={{}} src={sprite} />
+    <div
+      className={styles.content}
+      style={buttonStyle}
+    >
+      <img className={styles.image} src={sprite} />
     </div>
   );
 }
