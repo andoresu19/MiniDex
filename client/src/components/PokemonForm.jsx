32f823fa5102c099d751cd21408c412e78a433ee
andresbomo <andres.bomo02@gmail.com>
2023-01-21 09:38:57 -0500

    fix(pokemonform): chage description

--- client/src/components/PokemonForm.jsx
+++ client/src/components/PokemonForm.jsx
@@ -3,10 +3,16 @@ import styles from "./PokemonForm.module.css";
 export function PokemonForm() {
   return (
     <div className={styles.div_container}>
-      <h1 className={styles.title}>Mini-dex</h1>
-      <p className={styles.subtitle}>A portable mini web pokedex</p>
-      <input className={styles.input} type="text" />
-      <input className={styles.input} type="text" />
+      <div className={styles.text}>
+        <h1 className={styles.title}>Mini-dex</h1>
+        <p className={styles.subtitle}>
+          A small web version of the pokemon pokedex.
+        </p>
+      </div>
+      <div className={styles.inputs}>
+        <input className={styles.input} type="text"  placeholder="ID"/>
+        <input className={styles.input} type="text" placeholder="Name"/>
+      </div>
       <label className={styles.button_label} for="button_query">
         Hola
       </label>
