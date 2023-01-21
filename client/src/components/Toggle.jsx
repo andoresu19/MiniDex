e79679da23b221d8bc19d4076d7b702f55fa3fb3
andresbomo <andres.bomo02@gmail.com>
2023-01-21 09:38:19 -0500

    feat(toggle): add toggle component

---
+++ client/src/components/Toggle.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import styles from "./Toggle.module.css";
+
+export function Toggle() {
+  const [isToggle, setIsToggle] = useState(false);
+  const handleIcon = () => {
+    if (isToggle) {
+      setIsToggle(false);
+    } else {
+      setIsToggle(true);
+    }
+  };
+  useEffect(() => {});
+
+  return (
+    <div className={styles.switch}>
+      <input
+        type="checkbox"
+        id="toggle"
+        className={styles.check}
+        onChange={handleIcon}
+      />
+      <label
+        htmlFor="toggle"
+        className={isToggle ? styles.label : styles.labelDisabled}
+      >
+        <div className={styles.test} />
+      </label>
+    </div>
+  );
+}
