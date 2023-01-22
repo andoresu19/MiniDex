397ebdfadb65e98144c276902edc346ff501d9b2
andresbomo <andres.bomo02@gmail.com>
2023-01-22 14:47:11 -0500

    feat(pokemonform): add button animation

--- client/src/components/PokemonForm.jsx
+++ client/src/components/PokemonForm.jsx
@@ -1,36 +1,34 @@
 import styles from "./PokemonForm.module.css";
 import { InputText } from "./InputText";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export function PokemonForm({ theme, handleClick, pokemon }) {
-  const [idValue, setIdValue] = useState(pokemon.id)
-  const [nameValue, setNameValue] = useState(pokemon.name)
+  const [idValue, setIdValue] = useState(pokemon.id);
+  const [nameValue, setNameValue] = useState(pokemon.name);
 
   const handleTheme = (curr) =>
     curr === "light" ? styles.container : styles.containerDark;
 
   const handleId = (change) => {
-    if (change) 
-    {
-      setNameValue('')
-      setIdValue(change)
+    if (change) {
+      setNameValue("");
+      setIdValue(change);
     }
-    return nameValue
+    return nameValue;
   };
   const handleName = (change) => {
-    if (change) 
-    {
-      setIdValue('')
-      setNameValue(change)
+    if (change) {
+      setIdValue("");
+      setNameValue(change);
     }
-    return idValue
+    return idValue;
   };
 
   useEffect(() => {
-    setIdValue(pokemon.id)
-    setNameValue(pokemon.name)
-  }, [pokemon])
-
+    setIdValue(pokemon.id);
+    setNameValue(pokemon.name);
+  }, [pokemon]);
 
   return (
     <div className={handleTheme(theme)}>
@@ -43,21 +41,39 @@ export function PokemonForm({ theme, handleClick, pokemon }) {
         </p>
       </div>
       <div className={styles.inputs}>
-        <InputText theme={theme} number placeholder={"ID"} value={idValue} handle={handleId}/>
-        <InputText theme={theme} placeholder={"Name"} value={nameValue} handle={handleName}/>
+        <InputText
+          theme={theme}
+          number
+          placeholder={"ID"}
+          value={idValue}
+          handle={handleId}
+        />
+        <InputText
+          theme={theme}
+          placeholder={"Name"}
+          value={nameValue}
+          handle={handleName}
+        />
       </div>
       <label className={styles.button_label} htmlFor="button_query">
         Hola
       </label>
-      <button
+      <motion.button
         type="button"
+        whileHover={{
+          scale: 1.05,
+          transition: { duration: 0.25 },
+        }}
+        whileTap={{ scale: 0.9 }}
         id="button_query"
         name="button_query"
         className={styles.button}
-        onClick={() => {handleClick(idValue, nameValue)}}
+        onClick={() => {
+          handleClick(idValue, nameValue);
+        }}
       >
         Consultar
-      </button>
+      </motion.button>
     </div>
   );
 }
